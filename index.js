c5398a63dedc45e14b04149d86715c296f0da3c5
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 <anzymen@gmail.com>
2024-06-03 17:35:58 +0200

    added js feature for when hovered

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('photo gallery here!');
+const gallery = document.querySelector(".gallery");
+const boxOne = document.querySelector(".box-one");
+const boxTwo = document.querySelector(".box-two");
+const boxThree = document.querySelector(".box-three");
+const boxFour = document.querySelector(".box-four");
+
+const boxes = [boxOne, boxTwo, boxThree, boxFour];
+
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", function () {
+    box.classList.add("hovered");
+    const image = box.querySelector(".images");
+    if (image) {
+      image.classList.add("hovered-image");
+    }
+  });
+
+  box.addEventListener("mouseout", function () {
+    box.classList.remove("hovered");
+    const image = box.querySelector(".images");
+    if (image) {
+      image.classList.remove("hovered-image");
+    }
+  });
+});
