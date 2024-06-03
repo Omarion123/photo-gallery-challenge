5d7c27f200c24b058ae621d5f7ffbe7ef6bfa9f3
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 <anzymen@gmail.com>
2024-06-03 19:07:46 +0200

    added required filter

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const boxOne = document.querySelector(".box-one");
 const boxTwo = document.querySelector(".box-two");
 const boxThree = document.querySelector(".box-three");
 const boxFour = document.querySelector(".box-four");
+// const knowMore = document.querySelector(".know-more");
 
 const boxes = [boxOne, boxTwo, boxThree, boxFour];
 
@@ -10,16 +11,20 @@ boxes.forEach((box) => {
   box.addEventListener("mouseover", function () {
     box.classList.add("hovered");
     const image = box.querySelector(".images");
-    if (image) {
+    const knowMore = box.querySelector(".know-more");
+    if (image && knowMore) {
       image.classList.add("hovered-image");
+      knowMore.classList.add("know-more-hovered");
     }
   });
 
   box.addEventListener("mouseout", function () {
     box.classList.remove("hovered");
     const image = box.querySelector(".images");
-    if (image) {
+    const knowMore = box.querySelector(".know-more");
+    if (image && knowMore) {
       image.classList.remove("hovered-image");
+      knowMore.classList.remove("know-more-hovered");
     }
   });
 });
