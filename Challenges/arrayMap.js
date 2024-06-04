0860065b717dbebf67a7b2808356ead1473e335f
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 <anzymen@gmail.com>
2024-06-04 16:57:50 +0200

    added remaining constraints

--- Challenges/arrayMap.js
+++ Challenges/arrayMap.js
@@ -2,22 +2,31 @@ function hasContiguousSubarrayWithSum(arr, target) {
   let start = 0;
   let currentSum = arr[0];
 
-  for (let end = 1; end <= arr.length; end++) {
-    while (currentSum > target && start < end - 1) {
-      currentSum -= arr[start];
-      start++;
+  if (arr.length > 1 && arr.length < 100000) {
+    for (let end = 1; end <= arr.length; end++) {
+      if (
+        -Infinity < (currentSum, arr[end], target) &&
+        (currentSum, arr[end], target) < +Infinity
+      ) {
+        while (currentSum > target && start < end - 1) {
+          currentSum -= arr[start];
+          start++;
+        }
+
+        if (currentSum === target) {
+          return true;
+        }
+
+        if (end < arr.length) {
+          currentSum += arr[end];
+        }
+      }
     }
 
-    if (currentSum === target) {
-      return true;
-    }
-
-    if (end < arr.length) {
-      currentSum += arr[end];
-    }
+    return false;
+  } else {
+    return "invalid array length";
   }
-
-  return false;
 }
 
 const arr = [4, 2, 7, 1, 9, 5];
